7d04a1f1dbad37f75a7cbbaf7007798a596b27ef
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-09-13 20:59:55 +0300

    Create main.cpp

---
+++ lab_1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <triangle.h>
+
+using namespace std;
+
+int main()
+{
+    
+    Point 
+    Octagon a;
+    std::cout<<"The area of your octagon is " << a.Area()<<endl;
+    std::cout<<"The amount verices in your figure is "<< a.VertexesNumber()<<endl;
+    a.Print();
+
+    Triangle b;
+    std::cout<<"The area of your triangle is " << b.Area()<<endl;
+    std::cout<<"The amount verices in your figure is "<< b.VertexesNumber()<<endl;
+    b.Print();
+
+    Square c;
+    std::cout<<"The area of your square is " << c.Area()<<endl;
+    std::cout<<"The amount verices in your figure is "<< c.VertexesNumber()<<endl;
+    c.Print();
+
+    return 0;
+}
