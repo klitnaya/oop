ec76333290ab61395fa5b265b5fae28d3f4a496d
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-09-29 20:21:59 +0300

    Add files via upload

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include "point.h"
-#include "octagon.h"
 #include "triangle.h"
+#include "octagon.h"
 #include "square.h"
 
 using namespace std;
 
 int main()
 {
-    
-    Point 
-    Octagon a;
+    Octagon a(std::cin);
     std::cout<<"The area of your octagon is " << a.Area()<<endl;
     std::cout<<"The amount verices in your figure is "<< a.VertexesNumber()<<endl;
     a.Print();
 
-    Triangle b;
+    Triangle b(std::cin);
     std::cout<<"The area of your triangle is " << b.Area()<<endl;
     std::cout<<"The amount verices in your figure is "<< b.VertexesNumber()<<endl;
     b.Print();
 
-    Square c;
+    Square c(std::cin);
     std::cout<<"The area of your square is " << c.Area()<<endl;
     std::cout<<"The amount verices in your figure is "<< c.VertexesNumber()<<endl;
     c.Print();
