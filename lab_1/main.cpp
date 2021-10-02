4d631f30b15e49302242c6894a557b17b3d4786c
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-10-02 20:00:53 +0300

    Add files via upload

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -23,23 +23,5 @@ int main()
     std::cout << "The area of the square: " << s.Area() << std::endl;
     std::cout << std::endl;
 
-    
-/*    Square square(std::cin);
-    std::cout << "The number of square's vertexes: " << square.VertexesNumber() << std::endl;
-    square.Print(std::cout);
-    std::cout << "The area of the square: " << square.Area() << std::endl;
-    std::cout << std::endl;
-
-    Rectangle rectangle(std::cin);
-    std::cout << "The number of rectangle's vertexes: " << rectangle.VertexesNumber() << std::endl;
-    rectangle.Print(std::cout);
-    std::cout << "The area of the rectangle: " << rectangle.Area() << std::endl;
-    std::cout << std::endl;
-
-    Trapezoid trapezoid(std::cin);
-    std::cout << "The number of trapezoid's vertexes: " << trapezoid.VertexesNumber() << std::endl;
-    trapezoid.Print(std::cout);
-    std::cout << "The area of the trapezoid: " << trapezoid.Area() << std::endl;
-*/
     return 0;
 }
