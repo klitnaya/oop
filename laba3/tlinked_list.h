48a8e2e3921b0e5fdf556f705d7dcd6856f550cb
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-10-17 18:25:31 +0300

    Update tlinked_list.h

--- laba3/tlinked_list.h
+++ laba3/tlinked_list.h
@@ -1,6 +1,7 @@
 #ifdef TLINKED_LIST_H
 #define TLINKED_LIST_H
 #include "tlinked_list_item.h"
+#include"octagon.h"
 
 class TLinkedList {
 public:
@@ -8,21 +9,21 @@ public:
  int size_of_list;
  void Lenght();
  void empty();
- TLinkedList* First();
- TLinkedList* Last();
- Octagon* GetItem(size_t idx);
+ std::shared_ptr<TLinkedList>& First();
+ std::shared_ptr<TLinkedList>& Last();
+ std::shared_ptr<Octagon>& GetItem(size_t idx);
  void Empty();
- void InsertFirst(const Octagon &&octagon);
- void InsertLast(const Octagon &&octagon);
+ void InsertFirst(const std::shared_ptr<Octagon> &&octagon);
+ void InsertLast(const std::shared_ptr<Octagon> &&octagon);
  void RemoveLast();
  void RemoveFirst();
- void Insert(Octagon &&octagon, size_t position);
- void Remove(int a);
+ void Insert(const std::shared_ptr<Octagon> &&octagon, size_t position);
+ void Remove(size_t position);
  void Clear();
  friend std::ostream& operator<<(std::ostream& os, TLinkedList& list);
  ~TLinkedList();
 private:
- TLinkedList* front;
- TLinkedList* back;
+ std::shared_ptr<TLinkedList> front;
+ std::shared_ptr<TLinkedList> back;
 };
 #endif //TLINKED_LIST_H
