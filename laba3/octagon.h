18aa6563d9ea38af2b78fb09dc8fa7b2c9bf6677
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-10-17 18:46:53 +0300

    Update octagon.h

--- laba3/octagon.h
+++ laba3/octagon.h
@@ -1,6 +1,6 @@
 #ifndef OCTAGON_H
 #define OCTAGON_H
-
+#include<memory>
 #include "figure.h"
 
 class Octagon : public Figure{
@@ -8,13 +8,14 @@ public:
     Octagon();
     Octagon(std::istream& is);
     Octagon(Point point_a, Point point_b, Point point_c, Point point_d, Point point_e, Point point_f, Point point_g, Point point_h );
+    Octagon(std::shared_ptr<Octagon>& other);
     size_t VertexesNumber();
     double Area();
     void Print(std::ostream& os);
     virtual ~Octagon();
     Octagon& operator=(const Octagon& other);
     Octagon& operator==(const Octagon& other);
-    friend std::ostream& operator<<(std::ostream& os, Octagon& p);
+    friend std::ostream& operator<<(std::ostream& os,std::shared_ptr<Octagon>& p);
 
 private:
     Point point_a, point_b, point_c, point_d, point_e, point_f, point_g, point_h, ;
