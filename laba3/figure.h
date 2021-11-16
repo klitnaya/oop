151575eb87f32774a3c25eba9a5f68560c8354bd
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-11-16 19:28:57 +0300

    Update figure.h

--- laba3/figure.h
+++ laba3/figure.h
@@ -7,7 +7,7 @@ class Figure {
 public:
     virtual size_t VertexesNumber() = 0; 
     virtual double Area() = 0; 
-    virtual void Print(std::ostream& os) = 0;
+    //virtual void Print(std::ostream& os) = 0;
     virtual ~Figure() {};
 
 };
