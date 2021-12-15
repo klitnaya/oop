c4524f19bb3e01d0a11de490be6db155264e6f9c
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-12-15 20:33:04 +0300

    Update main.cpp

--- laba01/main.cpp
+++ laba01/main.cpp
@@ -5,7 +5,7 @@ int main(){
     Money c(std::cin);
     Money a(10, 6);
     Money b(12, 5);
-    std::cout << "Money objects"<< a << b << c << std::endl;
+    std::cout << "Money objects: "<< a << b << c << std::endl;
     std::cout << "Sum: " << a+b << std::endl;
     std::cout << "Division of residues " << a/b << std::endl;
     std::cout << "Multiplication of residuals " << a*b << std::endl;
