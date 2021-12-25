2909396c7263c791c5e1944e55f557e5fee4dc87
klitnaya <90642591+klitnaya@users.noreply.github.com>
2021-12-25 10:48:11 +0300

    Create main.cpp

---
+++ laba02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Money.h"
+
+int main(){
+    Money c(std::cin);
+    Money a(10, 6);
+    Money b(12, 5);
+    std::cout << "Money objects: "<< a << b << c << std::endl;
+    std::cout << "Sum: " << a+b << std::endl;
+    std::cout << "Division of residues " << a/b << std::endl;
+    std::cout << "Multiplication of residuals " << a*b << std::endl;
+    std::cout << "Sum " << c+b << std::endl;
+    std::cout << "Operator -- : " << --a;
+    std::cout << "Operator ++ : " << ++a;
+}
