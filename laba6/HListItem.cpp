4319566a51ed50b7b4d9635fc9a0fd733a4ce86d
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-05 14:13:53 +0300

    Create HListItem.cpp

---
+++ laba6/HListItem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "HListItem.h"
+
+template <class T> HListItem<T>::HListItem(const std::shared_ptr<Octagon> &octagon) {
+  this->octagon = octagon;
+  this->next = nullptr;
+}
+template <class A> std::ostream& operator<<(std::ostream& os,HListItem<A> &obj) {
+  os << "[" << obj.octagon << "]" << std::endl;
+  return os;
+}
+template <class T> HListItem<T>::~HListItem() {
+}
