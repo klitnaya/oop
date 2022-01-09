0ef17cfcb9d527fd0da5bdb18100629294f75194
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-09 18:27:11 +0300

    Update figure.h

--- laba2/figure.h
+++ laba2/figure.h
@@ -3,13 +3,11 @@
 
 #include "point.h"
 
-class Figure { 
+class Figure {
 public:
-    virtual size_t VertexesNumber() = 0; 
-    virtual double Area() = 0; 
-    //virtual void Print(std::ostream& os) = 0;
+    virtual double Area() = 0;
+    virtual size_t VertexesNumber() = 0;
     virtual ~Figure() {};
-
 };
 
 #endif // FIGURE_H
