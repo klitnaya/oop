9b9433539f8e9c67a8b371ba77421d744c595c5e
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-09 18:28:16 +0300

    Update point.h

--- laba3/point.h
+++ laba3/point.h
@@ -7,25 +7,21 @@
 
 class Point {
 public:
-    Point();
-    Point(std::istream& is);
-    Point(double x, double y);
+  Point();
+  Point(std::istream &is);
+  Point(double x, double y);
 
-    double dist(Point& other);
-    double X();
-    double Y();
+  double dist(Point& other);
+  
+  friend std::istream& operator>>(std::istream& is, Point& p);
+  friend std::ostream& operator<<(std::ostream& os, Point& p);
 
-
-    friend std::istream& operator>>(std::istream& is, Point& p);
-    friend std::ostream& operator<<(std::ostream& os, Point& p);
-
-    friend class Square;
-    friend class Octagon;
-    friend class Triangle;
+  double x();
+  double y();
 
 private:
-    double x_;
-    double y_;
+  double x_;
+  double y_;
 };
 
 #endif // POINT_H
