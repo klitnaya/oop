7310718992c738fd8b7dbe1e57593b0ac601cf09
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-09 21:18:13 +0300

    Add files via upload

--- laba4/figure.h
+++ laba4/figure.h
@@ -2,11 +2,13 @@
 #define FIGURE_H
 #include <iostream>
 #include "point.h"
+
 class Figure {
 public:
     virtual double Area() = 0;
     virtual size_t VertexesNumber() = 0;
     virtual ~Figure() {};
 };
+
 
 #endif // FIGURE_H
