419a9e4b11c1500d22e93cb6bb269083e36234b4
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-09 18:25:32 +0300

    Update main.cpp

--- laba2/main.cpp
+++ laba2/main.cpp
@@ -2,24 +2,23 @@
 #include "octagon.h"
 #include "tlinked_list.h"
 
-int main(){
+int main() {
   TLinkedList tlinkedlist;
   tlinkedlist.Empty();
-  tlinkedlist.InsertLast(Octagon(Point(1,2),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,10), Point(11,12), Point(12,13)));
-  tlinkedlist.InsertLast(Octagon(Point(13,14),Point(14,15),Point(15,16),Point(16,17),Point(17,18),Point(18,19),Point(19,20),Point(20,21)));
-  tlinkedlist.InsertLast(Octagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24), Point(25,26),Point(27,28)));
-  tlinkedlist.InsertLast(Octagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24), Point(25,26),Point(27,28)));
+  tlinkedlist.InsertLast(Octagon(Point(1,2),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,10), Point (11,12), Point(13,14)));
+  tlinkedlist.InsertLast(Octagon(Point(11,12),Point(12,13),Point(13,14),Point(14,15),Point(15,16),Point(16,17),Point(17,18), Point(18,19)));
+  tlinkedlist.InsertLast(Octagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24), Point(24,25),Point(25,26)));
+  tlinkedlist.InsertLast(Octagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24),Point(25,26), Point(27,28)));
   std::cout << tlinkedlist;
   tlinkedlist.RemoveLast();
   std::cout << tlinkedlist.Length() << std::endl;
   tlinkedlist.RemoveFirst();
   tlinkedlist.InsertFirst(Octagon(Point(2,3),Point(3,4),Point(4,5),Point(5,6),Point(6,7),Point(7,8), Point(8,9), Point(9,10)));
-  tlinkedlist.Insert(Octagon(Point(1,1),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,10), Point(11,12),Point(13,18)),2);
-  tlinkedlist.Empty();
+  tlinkedlist.Insert(Octagon(Point(1,1),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,10),Point(11,12),Point(13,16)),2);
+  std::cout << tlinkedlist.Empty() << std::endl;
   std::cout << tlinkedlist.First() << std::endl;
   std::cout << tlinkedlist.Last() << std::endl;
   std::cout << tlinkedlist.GetItem(2) << std::endl;
-  
   tlinkedlist.Remove(2);
   std::cout << tlinkedlist;
   tlinkedlist.Clear();
