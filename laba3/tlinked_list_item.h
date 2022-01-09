402d5221bbf1e497818b6bd47aedc5bb31ffc971
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-09 20:40:43 +0300

    Update tlinked_list_item.h

--- laba3/tlinked_list_item.h
+++ laba3/tlinked_list_item.h
@@ -2,13 +2,12 @@
 #include "octagon.h"
 #include<memery>
 
-class  HListItem {
+class HListItem {
 public:
-     HListItem(const std::shared_ptr<Octagon>& octagon);
-    friend std::ostream& operator<<(std::ostream& os,  std::shared_ptr<HListItem>& obj);
-    ~ HListItem();
-    std::shared_ptr<HListItem> next;
-    std::shared_ptr<Octagon> octagon;
-    
+  HListItem(const std::shared_ptr<Octagon> &octagon);
+  friend std::ostream& operator<<(std::ostream& os, std::shared_ptr<HListItem>&& obj);
+  ~HListItem();
+  std::shared_ptr<Octagon> octagon;
+  std::shared_ptr<HListItem> next;
 };
 
