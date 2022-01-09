679cbd7d24d0b4a56f3381e4ec351671a3b0d760
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-09 20:45:09 +0300

    Update tlinked_list.h

--- laba3/tlinked_list.h
+++ laba3/tlinked_list.h
@@ -6,26 +6,25 @@
 
 class TLinkedList {
 public:
- TLinkedList();
- int size_of_list;
- size_t Lenght();
- bool Empty();
- std::shared_ptr<Octagon>& First();
- std::shared_ptr<Octagon>& Last();
- std::shared_ptr<Octagon>& GetItem(size_t idx);
- //void Empty();
- TLinkedList(const std::shared_ptr<TLinkedList> &other);
- void InsertFirst(const std::shared_ptr<Octagon> &&octagon);
- void InsertLast(const std::shared_ptr<Octagon> &&octagon);
- void RemoveLast();
- void RemoveFirst();
- void Insert(const std::shared_ptr<Octagon> &&octagon, size_t position);
- void Remove(size_t position);
- void Clear();
- friend std::ostream& operator<<(std::ostream& os, TLinkedList& list);
- ~TLinkedList();
+  TLinkedList();
+  int size_of_list;
+  size_t Length();
+  std::shared_ptr<Octagon>& First();
+  std::shared_ptr<Octagon>& Last();
+  std::shared_ptr<Octagon>& GetItem(size_t idx);
+  bool Empty();
+  TLinkedList(const std::shared_ptr<TLinkedList> &other);
+  void InsertFirst(const std::shared_ptr<Octagon> &&octagon);
+  void InsertLast(const std::shared_ptr<Octagon> &&octagon);
+  void RemoveLast();
+  void RemoveFirst();
+  void Insert(const std::shared_ptr<Octagon> &&octagon, size_t position);
+  void Remove(size_t position);
+  void Clear();
+  friend std::ostream& operator<<(std::ostream& os, TLinkedList& list);
+  ~TLinkedList(); 
 private:
- std::shared_ptr<HListItem> front;
- std::shared_ptr<HListItem> back;
+  std::shared_ptr<HListItem> front;
+  std::shared_ptr<HListItem> back;
 };
 #endif //TLINKED_LIST_H
