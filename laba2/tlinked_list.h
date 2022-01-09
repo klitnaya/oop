78ec19ab44d9a06507e9986e44343bba4c2c3a91
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-09 18:24:04 +0300

    Update tlinked_list.h

--- laba2/tlinked_list.h
+++ laba2/tlinked_list.h
@@ -6,26 +6,26 @@
 
 class TLinkedList {
 public:
- TLinkedList();
- int size_of_list;
- size_t Lenght();
- bool Empty();
- TLinkedList& First();
- TLinkedList& Last();
- TLinkedList(const TLinkedList& other);
- Octagon& GetItem(size_t idx);
- //void Empty();
- void InsertFirst(const Octagon &&octagon);
- void InsertLast(const Octagon &&octagon);
- void RemoveLast();
- void RemoveFirst();
- void Insert(Octagon &&octagon, size_t position);
- void Remove(size_t position);
- void Clear();
- friend std::ostream& operator<<(std::ostream& os, TLinkedList& list);
- ~TLinkedList();
+  TLinkedList();
+  int size_of_list;
+  size_t Length();
+  bool Empty();
+  Octagon& First();
+  Octagon& Last();
+  TLinkedList(const TLinkedList& other);
+  Octagon& GetItem(size_t idx);
+  void InsertFirst(const Octagon &&octagon);
+  void InsertLast(const Octagon &&octagon);
+  void RemoveLast();
+  void RemoveFirst();
+  void Insert(const Octagon &&octagon, size_t position);
+  void Remove(size_t position);
+  void Clear();
+  friend std::ostream& operator<<(std::ostream& os, TLinkedList& list);
+  ~TLinkedList(); 
 private:
- HListItem *front;
- HListItem *back;
+  HListItem *front;
+  HListItem *back;
 };
+
 #endif //TLINKED_LIST_H
