2cb563bd514b900a7ee696db2e0e7940d5505f6c
klitnaya <90642591+klitnaya@users.noreply.github.com>
2022-01-10 18:36:54 +0300

    Update TAllocatorBlock.h

--- laba6/TAllocatorBlock.h
+++ laba6/TAllocatorBlock.h
@@ -2,56 +2,24 @@
 #define TALLOCATORBLOCK_H
 
 #include "TLinkedList.h"
+#include "SpehelList.h"
 #include <memory>
 
-class TAllocatorBlock {
-    public:
-        TAllocatorBlock(const size_t& size, const size_t count){
-            this->size = size;
-            for(int i = 0; i < count; ++i){
-                unused_blocks.Insert(malloc(size));
-            }
-        }
-        void* Allocate(const size_t& size){
-            if(size != this->size){
-                std::cout << "Error during allocation\n";
-            }
-            if(unused_blocks.Length()){
-                for(int i = 0; i < 5; ++i){
-                    unused_blocks.Insert(malloc(size));
-                }
-            }
-            void* tmp = unused_blocks.GetItem(1);
-            used_blocks.Insert(unused_blocks.GetItem(1));
-            unused_blocks.Remove(0);
-            return tmp;
-        }
-        void Deallocate(void* ptr){
-            unused_blocks.Insert(ptr);
-        }
-    ~TAllocatorBlock(){
-        while(used_blocks.size()){
-            try{
-                free(used_blocks.GetItem(1);)
-                used_blocks.Remove(0);
-            } catch(...){
-                used_blocks.Remove(0);
-            }
-        }
-        while(unused_blocks.size()){
-            try{
-                free(unused_blocks.GetItem(1);
-                unused_blocks.Remove(0);
-            } catch(...){
-                unused_blocks.Remove(0);
-            }
-        }
-    }
+class TAllocationBlock {
+ public:
+  TAllocationBlock(size_t size, size_t count);
+  void* allocate();
+  void deallocate(void* pointer);
+  bool has_free_blocks();
 
-    private:
-        size_t size;
-        TLinkedList <void*> used_blocks;
-        TLinkedList <void*> unused_blocks;
+  virtual ~TAllocationBlock();
+
+ private:
+  size_t _size;
+  size_t _count;
+  char* _used_blocks;
+  SpehelList<void*> vec_free_blocks;
+  size_t _free_count;
 };
 
 #endif
